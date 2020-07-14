7bc1962c9a2d2b1b1ecf679e50f1990b45597ea1
gfrednand <gfrednand@gmail.com>
2020-07-14 14:34:21 +0300

    changes

---
+++ build/precache-manifest.b691e2a5ada986285502474d02ef31bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ee8fe1573eccea0bc97f5a6c76445c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b052a4ab69b0e11b8c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b052a4ab69b0e11b8c",
+    "url": "/static/js/main.2ad1caae.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
